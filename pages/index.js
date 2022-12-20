ff64d9a953a040b7539e28ec69e77981412ffebd
Kartik <rai.kartik99@gmail.com>
2022-12-20 22:08:30 +0530

    updated

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,9 @@ export const getServerSideProps = async (ctx) => {
     admin = true;
   }
 
-  const res = await axios.get("http://localhost:3000/api/products");
+  const res = await axios.get(
+    "https://next-js-pizza-app-kartik990.vercel.app/api/products"
+  );
   return {
     props: {
       pizzaList: res.data,
